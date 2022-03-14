68f0913d44da134185cc27db2a1feb150af5cd79
Barbarubia <dnl.desimone@gmail.com>
2022-03-14 10:18:18 -0300

    Stampata lista dei todo

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        arrTodo: [
+            {
+                text: 'Comprare il biglietto aereo',
+                done: true
+            },
+            {
+                text: 'Verificare la documentazione necessaria per il viaggio',
+                done: true
+            },
+            {
+                text: 'Comprare souvenirs',
+                done: false
+            },
+            {
+                text: 'Preparare la valigia',
+                done: false
+            },
+            {
+                text: 'Fare il check-in',
+                done: false
+            },
+            {
+                text: 'Stampare il biglietto aereo',
+                done: false
+            },
+            {
+                text: 'Trovare un mezzo di trasporto per andare all\'aeroporto',
+                done: false
+            },
+        ]
+    }
+
+
+
+
+})
